f2862a788749878cc8aaaccb04c00d65bc42eb6c
arifmia1129 <arifmia1129@gmail.com>
2023-09-06 15:49:45 +0600

    readme file added

---
+++ README.md
@@ -0,0 +1,35 @@
+## Live Link: https://book-catalog-backend-with-prisma-phi.vercel.app
+
+## Application Routes:
+
+### User
+
+- api/v1/auth/signup (POST)
+- api/v1/users (GET)
+- api/v1/users/:id (Single GET) Include an id that is saved in your database
+- api/v1/users/:id (PATCH)
+- api/v1/users/:id (DELETE) Include an id that is saved in your database
+- api/v1/profile (GET)
+
+### Category
+
+- api/v1/categories/create-category (POST)
+- api/v1/categories (GET)
+- api/v1/categories/:id (Single GET) Include an id that is saved in your database
+- api/v1/categories/:id (PATCH)
+- api/v1/categories/:id (DELETE) Include an id that is saved in your database
+
+### Books
+
+- api/v1/books/create-book (POST)
+- api/v1/books (GET)
+- api/v1/books/:categoryId/category (GET)
+- api/v1/books/:id (GET)
+- api/v1/books/:id (PATCH)
+- api/v1/books/:id (DELETE)
+
+### Orders
+
+- api/v1/orders/create-order (POST)
+- api/v1/orders (GET)
+- api/v1/orders/:orderId (GET)
--- src/errors/handleMongoServerError.ts
+++ src/errors/handleMongoServerError.ts
@@ -1,21 +1,21 @@
-import mongoose from "mongoose";
-import { ErrorMessage } from "../interfaces/error.interface";
+// import mongoose from "mongoose";
+// import { ErrorMessage } from "../interfaces/error.interface";
 
-const handleMongoServerError = (err: mongoose.Error): ErrorMessage[] => {
-  const errors: ErrorMessage[] = [];
+// const handleMongoServerError = (err: mongoose.Error): ErrorMessage[] => {
+//   const errors: ErrorMessage[] = [];
 
-  if (err instanceof mongoose.mongo.MongoServerError) {
-    const fieldValues = Object.entries(err.keyValue);
+//   if (err instanceof mongoose.mongo.MongoServerError) {
+//     const fieldValues = Object.entries(err.keyValue);
 
-    fieldValues.forEach(([field, value]) => {
-      errors.push({
-        path: field,
-        message: `${value} is already taken as ${field} value`,
-      });
-    });
-  }
+//     fieldValues.forEach(([field, value]) => {
+//       errors.push({
+//         path: field,
+//         message: `${value} is already taken as ${field} value`,
+//       });
+//     });
+//   }
 
-  return errors;
-};
+//   return errors;
+// };
 
-export default handleMongoServerError;
+// export default handleMongoServerError;
